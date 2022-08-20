aa16e2dd31ef74cae6d836915c2c7941b4404e45
aleksamatovic <lexamatovic@gmail.com>
2022-08-20 21:36:24 +0200

    Init

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,11 +1,9 @@
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
-import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
