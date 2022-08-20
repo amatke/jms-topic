33f3bc76145a62495baaff2423cc797a4e16454e
Aleksa Matovic <lexamatovic@gmail.com>
2022-08-20 21:27:12 +0200

    Update Consumer.java

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -27,7 +27,7 @@ public class Consumer {
 
 			Topic topic = session.createTopic("demo-topic");
 
-			MessageConsumer consumer = session.createDurableSubscriber(topic, "Consumer-1");
+			MessageConsumer consumer = session.createDurableSubscriber(topic, "Subscriber-1");
 			consumer.setMessageListener(new MessageListener() {
 
 				public void onMessage(Message message) {
